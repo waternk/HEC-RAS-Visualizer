2af1ff1e66d1e49617f668ae6c5e72fb8f3a8194
Filip Tubic <tubefilip@gmail.com>
2016-08-26 18:05:38 +0200

    cross platform module

--- routes/geometry.ts
+++ routes/geometry.ts
@@ -2,22 +2,31 @@ import { Request, Response } from  "express";
 import * as child_process from 'child_process';
 import * as fs from 'fs';
 import * as path from 'path';
+import * as os from "os";
 
 export function geometry(req: any, res: Response) 
-{   
-    child_process.exec("cd HEC-RAS-Geometry-file-parser && ./hec /Users/filiptubic/Downloads/GRF-Ustaljeno18122015/DjerdapTestovi-GRF.g07", (error, stdout, stderr) => 
+{
+    if(os.platform() == "win32")
     {
-        if (error !== null)
+        
+    }
+    else
+    {
+        child_process.exec("cd HEC-RAS-Geometry-file-parser && ./hec /Users/filiptubic/Downloads/GRF-Ustaljeno18122015/DjerdapTestovi-GRF.g07", (error, stdout, stderr) => 
         {
-            console.log(error);
-            res.statusMessage = error.message;
-            res.status(500).end();
-        }
-        else
-        {
-            res.sendFile(path.join(__dirname, '../HEC-RAS-Geometry-file-parser/finalno.txt'));
-        }  
-    });
+            if (error !== null)
+            {
+                console.log(error);
+                res.statusMessage = error.message;
+                res.status(500).end();
+            }
+            else
+            {
+                res.sendFile(path.join(__dirname, '../HEC-RAS-Geometry-file-parser/finalno.txt'));
+            }  
+        });
+    }   
+    
 };
 
 export function uploadGeometry(req: any, res: Response)
